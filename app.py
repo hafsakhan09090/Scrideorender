0719f24848fa2cd607e9e9c471d574670c057c47
Hafsa Khan <hafsakhan09090@gmail.com>
2025-10-21 15:48:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, send_from_directory
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route("/")
-def serve_index():
+def home():
     return send_from_directory('.', 'index.html')
 
 if __name__ == '__main__':
